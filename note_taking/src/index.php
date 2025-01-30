9c837dbb5a99e2090a89d641cd96a77e60c55b6a
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2025-01-30 23:23:50 +0530

    initialised project files and the homepage and learned how to use php server

---
+++ note_taking/src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>Take Notes Here!</h1>
+        <a href=login.php>
+            login
+        </a>
+        <br>
+        <a href=register.php>
+            register
+        </a>
+    </body>
+</html>
---
+++ note_taking/src/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <h1>Login</h1>
+        Enter your username
+        <br>
+        
+    </body>
+</html>
